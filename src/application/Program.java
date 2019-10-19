f7ce43b0e09fdbe662bc64ad7a5d51ebf4b23c4d
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-19 16:56:52 -0300

    Created project - MyFunction.

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		showProduct(list);
+		
+	}
+	public static void showProduct(List<Product> list) {
+		list.sort((p1,p2)-> p1.getPrice().compareTo(p2.getPrice()));
+		for (Product p : list) {
+			System.out.println(p);
+		}
+		System.out.println();
+	}
+}
